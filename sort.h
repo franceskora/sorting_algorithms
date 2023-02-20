e5d25fabcef5925e57c181519faedb39763a7d9e
franceskora <francesanyakora@gmail.com>
2023-02-20 05:39:30 -0800

    my commit

--- sort.h
+++ sort.h
@@ -12,61 +12,32 @@
  * @prev: Pointer to the previous element of the list
  * @next: Pointer to the next element of the list
  */
-
 typedef struct listint_s
-
 {
-
-		int n;
-
-			struct listint_s *prev;
-
-				struct listint_s *next;
-
+	int n;
+	struct listint_s *prev;
+	struct listint_s *next;
 } listint_t;
 
 
-
-
-
 /* print.c */
-
 void print_array(const int *array, size_t size);
-
 void print_list(const listint_t *list);
-
 void check_list(const listint_t *list);
-
 void check_array(const int *array, size_t size);
-
 int *rand_array(int len, int max);
 
-
-
 void bubble_sort(int *array, size_t size);
-
 void insertion_sort_list(listint_t **list);
-
 void selection_sort(int *array, size_t size);
-
 void quick_sort(int *array, size_t size);
-
 void shell_sort(int *array, size_t size);
-
 void cocktail_sort_list(listint_t **list);
-
 void merge_sort(int *array, size_t size);
-
 void radix_sort(int *array, size_t size);
-
 void counting_sort(int *array, size_t size);
-
 void heap_sort(int *array, size_t size);
-
 void bitonic_sort(int *array, size_t size);
-
 void quick_sort_hoare(int *array, size_t size);
-
-
 
 #endif
